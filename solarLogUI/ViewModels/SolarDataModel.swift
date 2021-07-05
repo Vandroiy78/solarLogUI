1c54702f6a407bde01dea042ea229662be318af0
Holger Preu <holger@preu.ch>
2021-07-05 08:06:29 +0200

    - first working version

---
+++ solarLogUI/ViewModels/SolarDataModel.swift
@@ -0,0 +1,107 @@
+//
+//  SolarDataModel.swift
+//  solarLogUI
+//
+//  Created by Holger Preu on 10.06.21.
+//
+
+import Foundation
+
+class SolarDataModel: ObservableObject {
+    
+    @Published var solarData:SolarData = SolarData()
+    
+    init() {
+        
+        // Fetch Solar Data
+        getRemoteJsonFile()
+    }
+    
+    func updateData() {
+        getRemoteJsonFile()
+    }
+    
+    func getRemoteJsonFile() {
+        
+        // var myData:SolarData = SolarData()
+        
+        // Get a URL object
+        let url = URL(string: Constants.Network.url)
+        
+        guard url != nil else {
+            print("Could not create the URL object")
+            return
+        }
+        
+        // Get a URL Session object
+        let session = URLSession.shared
+        
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Build the JSON query
+        let jsonQuery: [String:Any] = ["801" : ["170" : nil]]
+        
+        // Serialize the array to JSON data
+        var jsonTodo : Data
+        
+        do {
+            jsonTodo = try JSONSerialization.data(withJSONObject: jsonQuery, options: [])
+            
+            // Get a data task object
+            let dataTask = session.uploadTask(with: request, from: jsonTodo) { (data, urlreponse, error) in
+                
+                // Check that there wasn't an error
+                if error == nil && data != nil {
+                    do {
+                        
+                        // Create a JSON Decoder object
+                        let decoder = JSONDecoder()
+                        
+                        // Parse the JSON
+                        let myDecodedData = try decoder.decode(Welcome.self, from: data!)
+                        print(Thread.isMainThread)
+                        if let container = myDecodedData.the801 {
+                            self.solarData = container.the170
+                        }
+                    }
+                    
+                    catch {
+                        print("Could not parse the JSON response")
+                    }
+                }
+            }
+            
+            /*
+            let dataTask2 = session.dataTaskPublisher(for: request)
+                .map { $0.data
+                    
+                    
+                }
+                .decode(type: Welcome.self, decoder: JSONDecoder())
+                .receive(on: RunLoop.main)
+            
+            let cancellableSink = dataTask2
+                .sink(receiveCompletion: { completion in
+                        print(".sink() received the completion", String(describing: completion))
+                        switch completion {
+                            case .finished:
+                                break
+                            case .failure(let anError):
+                                print("received error: ", anError)
+                        }
+                }, receiveValue: { someValue in
+                    print(".sink() received \(someValue)")
+                })*/
+            
+            // Call resume on the data task
+            dataTask.resume()
+        }
+        catch {
+            print ("Cannot encode JSON")
+        }
+        
+    }
+}
+
--- solarLogUI/Views/solarStatView.swift
+++ solarLogUI/Views/solarStatView.swift
@@ -10,13 +10,14 @@ import SwiftUI
 struct solarStatView: View {
     
     @EnvironmentObject var model:SolarDataModel
+    let myGreen = Color(.sRGB, red: 0, green: 0.7, blue: 0.2, opacity: 1.0)
     
     var body: some View {
         VStack {
             Text("SolarLog")
                 .font(.largeTitle)
                 .padding()
-            Text("Installiert:")
+            Text("Installiert: " + prettyPrint(model.solarData.totalPower, "W"))
                 .padding()
             HStack {
                 VStack {
@@ -24,16 +25,23 @@ struct solarStatView: View {
                     Text("Heute:")
                 }
                 VStack(alignment: .trailing) {
-                    Text("4.00 kW")
+                    Text(prettyPrint(model.solarData.pdc, "W"))
                     Text("2.31 kWh")
                 }
                 VStack(alignment: .trailing) {
-                    Text("2.69 kW")
+                    Text(prettyPrint(model.solarData.consPac, "W"))
+                    Text("1.34 kWh")
+                }
+                VStack(alignment: .trailing) {
+                    let balance =  model.solarData.pdc - model.solarData.consPac
+                    
+                    Text(prettyPrint(balance, "W"))
+                        .foregroundColor(balance >= 0 ? myGreen : .red)
                     Text("1.34 kWh")
                 }
             }
             Button(action: {
-                
+                model.updateData()
             }, label: {
                 HStack {
                     Image(systemName: "arrow.triangle.2.circlepath")
@@ -41,21 +49,50 @@ struct solarStatView: View {
                 }
             })
             .padding()
-            Text("Daten von: ")
+            Text("Daten von: \(model.solarData.lastUpdateTime) ")
                 .font(.footnote)
+        }
+    }
+    
+    func prettyPrint(_ number:Int, _ unit:String) -> String {
+        
+        var myReturn: String
+        
+        if abs(number) < 1000 {
+            myReturn = String("\(number) \(unit)")
+        }
+        else if abs(number) < 1000000 {
+            let roundedNumber = (Double(number)/1000.0 * 100).rounded() / 100
+            myReturn = String("\(roundedNumber) k\(unit)")
+        }
+        else {
+            let roundedNumber = (Double(number)/1000000.0 * 100).rounded() / 100
+            myReturn = String("\(roundedNumber) M\(unit)")
+        }
+
+        return myReturn
+    }
+    
+    func printPowerValue(_ label:UILabel, _ prefix:String, _ value:Int, _ unit:String) {
+        label.text = "\(prefix): \(prettyPrint(value, unit))"
+    }
+    
+    func printBalance(_ label:UILabel, yield:Int, cons:Int, unit:String) {
+        var balance:Int
+        balance = yield - cons
+        label.text = prettyPrint(balance, unit)
+        if balance >= 0 {
+            label.textColor = UIColor(red: 0, green: 0.7, blue: 0.2, alpha: 1.0)
+        }
+        else {
+            label.textColor = .red
         }
     }
 }
 
 struct solarStatView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            solarStatView()
-                .preferredColorScheme(.dark)
-                .environmentObject(SolarDataModel())
-            solarStatView()
-                .preferredColorScheme(.light)
-                .environmentObject(SolarDataModel())
-        }
+        solarStatView()
+            .environmentObject(SolarDataModel())
     }
 }
