0a45443697f3b7b3dc230b627a816968d9f9894c
Holger Preu <holger@preu.ch>
2021-07-05 10:36:17 +0200

    - add all values to UI

--- solarLogUI/Views/solarStatView.swift
+++ solarLogUI/Views/solarStatView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct solarStatView: View {
     
     @EnvironmentObject var model:SolarDataModel
-    let myGreen = Color(.sRGB, red: 0, green: 0.7, blue: 0.2, opacity: 1.0)
     
     var body: some View {
         VStack {
@@ -20,26 +19,43 @@ struct solarStatView: View {
             Text("Installiert: " + prettyPrint(model.solarData.totalPower, "W"))
                 .padding()
             HStack {
-                VStack {
+                VStack(alignment: .leading) {
                     Text("Jetzt:")
                     Text("Heute:")
+                    Text("Gestern:")
+                    Text("Monat:")
+                    Text("Jahr:")
+                    Text("Gesamt:")
                 }
                 VStack(alignment: .trailing) {
                     Text(prettyPrint(model.solarData.pdc, "W"))
-                    Text("2.31 kWh")
+                    Text(prettyPrint(model.solarData.yieldDay, "Wh"))
+                    Text(prettyPrint(model.solarData.yieldYesterday, "Wh"))
+                    Text(prettyPrint(model.solarData.yieldMonth, "Wh"))
+                    Text(prettyPrint(model.solarData.yieldYear, "Wh"))
+                    Text(prettyPrint(model.solarData.yieldTotal, "Wh"))
                 }
                 VStack(alignment: .trailing) {
                     Text(prettyPrint(model.solarData.consPac, "W"))
-                    Text("1.34 kWh")
+                    Text(prettyPrint(model.solarData.consYieldDay, "Wh"))
+                    Text(prettyPrint(model.solarData.consYieldYesterday, "Wh"))
+                    Text(prettyPrint(model.solarData.consYieldMonth, "Wh"))
+                    Text(prettyPrint(model.solarData.consYieldYear, "Wh"))
+                    Text(prettyPrint(model.solarData.consYieldTotal, "Wh"))
                 }
                 VStack(alignment: .trailing) {
-                    let balance =  model.solarData.pdc - model.solarData.consPac
-                    
-                    Text(prettyPrint(balance, "W"))
-                        .foregroundColor(balance >= 0 ? myGreen : .red)
-                    Text("1.34 kWh")
+                    printBalance(yield: model.solarData.pdc, cons: model.solarData.consPac, unit: "W")
+                    printBalance(yield: model.solarData.yieldDay, cons: model.solarData.consYieldDay, unit: "Wh")
+                    printBalance(yield: model.solarData.yieldYesterday, cons: model.solarData.consYieldYesterday, unit: "Wh")
+                    printBalance(yield: model.solarData.yieldMonth, cons: model.solarData.consYieldMonth, unit: "Wh")
+                    printBalance(yield: model.solarData.yieldYear, cons: model.solarData.consYieldYear, unit: "Wh")
+                    printBalance(yield: model.solarData.yieldTotal, cons: model.solarData.consYieldTotal, unit: "Wh")
                 }
             }
+            .padding()
+            .background(Color(.gray).opacity(0.2))
+            .cornerRadius(10)
+            
             Button(action: {
                 model.updateData()
             }, label: {
@@ -54,6 +70,7 @@ struct solarStatView: View {
         }
     }
     
+    // converts a number to 'kilo' or 'mega' if applicable and displays it with the according unit (with k or M as prefix)
     func prettyPrint(_ number:Int, _ unit:String) -> String {
         
         var myReturn: String
@@ -73,20 +90,15 @@ struct solarStatView: View {
         return myReturn
     }
     
-    func printPowerValue(_ label:UILabel, _ prefix:String, _ value:Int, _ unit:String) {
-        label.text = "\(prefix): \(prettyPrint(value, unit))"
-    }
-    
-    func printBalance(_ label:UILabel, yield:Int, cons:Int, unit:String) {
+    // print the balance including unit
+    // set textcolor to green if the balance is greater or equal to zero, else set it to red
+    func printBalance(yield:Int, cons:Int, unit:String) -> some View {
         var balance:Int
+        let myGreen = Color(.sRGB, red: 0, green: 0.7, blue: 0.2, opacity: 1.0)
+        
         balance = yield - cons
-        label.text = prettyPrint(balance, unit)
-        if balance >= 0 {
-            label.textColor = UIColor(red: 0, green: 0.7, blue: 0.2, alpha: 1.0)
-        }
-        else {
-            label.textColor = .red
-        }
+        return Text(prettyPrint(balance, unit))
+            .foregroundColor(balance >= 0 ? myGreen : .red)
     }
 }
 
